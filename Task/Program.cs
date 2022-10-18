70568e308131e90e8d163001cbda7796a66c61de
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2022-10-18 16:00:10 +0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Task
+{
+    class Program
+    {
+        static void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+        static void insertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        static void selectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 64, 34, 125, 12, 22, 118, 90 };
+            int[] arr1 = { 634, 4, 25, 12, 284, 151, 15  };
+            int[] arr2 = { 54, 34, 2, 12, 22, 11, 90 };
+
+            bubbleSort(arr);
+            insertionSort(arr1);
+            selectionSort(arr2);
+            Console.WriteLine("Sorted array");
+            printArray(arr);
+            printArray(arr1);
+            printArray(arr2);
+
+        }
+    }
+}
